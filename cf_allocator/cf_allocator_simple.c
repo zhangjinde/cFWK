98375163442afea30b5fcb1cdd9b50393958b263
Your Name <you@example.com>
2020-05-11 06:13:20 +0800

    xxx

--- cf_allocator/cf_allocator_simple.c
+++ cf_allocator/cf_allocator_simple.c
@@ -1,12 +1,22 @@
 #include "cf_allocator_simple.h"
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 static size_t mem_alloc_size = 0;
 void* cf_allocator_simple_alloc(size_t size){
     mem_alloc_size+=size;
     void* mem = calloc(1,size+sizeof(size_t));
     *((size_t*)mem) = size;
     return (uint8_t*)mem+sizeof(size_t);
+}
+void* cf_allocator_simple_clone(void* src,size_t size){
+    mem_alloc_size+=size;
+    void* mem = malloc(size+sizeof(size_t));
+    *((size_t*)mem) = size;
+    uint8_t* new_buffer = (uint8_t*)mem+sizeof(size_t);
+    memcpy(new_buffer,src,size);
+    return new_buffer;
+
 }
 void cf_allocator_simple_free(void* mem){
     mem_alloc_size -= *((size_t*)((uint8_t*)mem-sizeof(size_t)));
--- cf_allocator/cf_allocator_simple.h
+++ cf_allocator/cf_allocator_simple.h
@@ -7,6 +7,7 @@ extern "C" {
 #endif
 
 void* cf_allocator_simple_alloc(size_t size);
+void* cf_allocator_simple_clone(void* src,size_t size);
 void* cf_allocator_simple_realloc(void* mem,size_t size);
 void cf_allocator_simple_free(void* mem);
 size_t cf_allocator_alloc_size();
